811f44831d343f46a833645a469d4c09a66bee55
Kharag <kharag94@gmail.com>
2024-10-22 22:07:01 -0400

    product name

--- FactoryManager.py
+++ FactoryManager.py
@@ -8,3 +8,5 @@ elif productcode > 1000:
     print("input more than 1000. Input invalid")
 else:
     print("product code accepted")
+# The product name
+productname = input("Enter the name of the product :")
