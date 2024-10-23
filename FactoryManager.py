6c7b208588f015d29417b561c2d797bba0a0fe6b
Kharag <kharag94@gmail.com>
2024-10-22 22:08:17 -0400

    some changes

--- FactoryManager.py
+++ FactoryManager.py
@@ -4,8 +4,10 @@ print("Welcome To Programming Principles Factory Manager Program :)")
 productcode = int(input("Enter the product code: "))
 if productcode < 100 :
     print("input less than 100. Input invalid")
+    Exit()
 elif productcode > 1000:
     print("input more than 1000. Input invalid")
+    Exit()
 else:
     print("product code accepted")
 # The product name
